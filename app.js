3032f848c212902db3a2e8e4b131e411bc2c6593
Tu Nombre <tu_email@ejemplo.com>
2025-12-18 00:08:45 -0400

    Add Passenger entrypoints (app.js, passenger_app.js)

---
+++ app.js
@@ -0,0 +1,3 @@
+// Passenger/cPanel default entrypoint for Node apps.
+// Keep this file tiny: it delegates to the real server in index.js.
+require('./index');
---
+++ passenger_app.js
@@ -0,0 +1,3 @@
+// Some Passenger setups look specifically for passenger_app.js.
+// Delegate to app.js (which delegates to index.js).
+require('./app');
